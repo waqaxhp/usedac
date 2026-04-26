ad25b2d339a881ab2b9bae2486876f05a61207a2
waqaxhp <116118297+waqaxhp@users.noreply.github.com>
2026-04-26 09:22:57 +0500

    Update .

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,69 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Components/Home";
+import App from "./App";
+import Courses from "./Components/Courses";
+import ContactUs from "./Components/ContactUs";
+import Accordion from "./Components/Accordion";
+import PricingPlans from "./Components/PricingPlans";
+import LiveQuranTeachers from "./Components/LiveQuranTeachers";
+import DigitalLibrary from "./Components/DigitalLibrary";
+import IslamicLearningSection from "./Components/IslamicLearningSection";
+import FemaleQuranTeacher from "./Components/FemaleQuranTeacher";
+import MaleQuranTeacher from "./Components/MaleQuranTeacher";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/digital-library",
+        element: <IslamicLearningSection />,
+      },
+      {
+        path: "/downlode",
+        element: <DigitalLibrary />,
+      },
+
+      {
+        path: "courses",
+        element: <Courses />,
+      },
+      {
+        path: "/contact-us",
+        element: <ContactUs />,
+      },
+      {
+        path: "/testimonials",
+        element: <Accordion />,
+      },
+      {
+        path: "/hadiya",
+        element: <PricingPlans />,
+      },
+      {
+        path: "/about",
+        element: <LiveQuranTeachers />,
+      },
+      {
+        path: "/teachers",
+        element: <MaleQuranTeacher />,
+      },
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
